75e140f915b3a0f665017eab5920ca115b4e1956
imm92felgmail.com <imm92felgmail.com>
2018-10-05 10:21:37 +0200

    modificado

---
+++ AplicacionCorredoresV1/src/logica/GestionCorredor.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica;
+
+import java.util.ArrayList;
+import java.util.List;
+import modelo.Corredor;
+
+/**
+ *
+ * @author alumnop
+ */
+public class GestionCorredor {
+    
+    private List<Corredor> listaCorredores = new ArrayList<Corredor>();
+
+    public GestionCorredor() {
+    }
+
+    public List<Corredor> getListaCorredores() {
+        return listaCorredores;
+    }
+
+    public void setListaCorredores(List<Corredor> listaCorredores) {
+        this.listaCorredores = listaCorredores;
+    }
+
+
+    @Override
+    public String toString() {
+        return "GestionCorredor{" + "listaCorredores=" + listaCorredores + '}';
+    }
+    
+    
+    
+    
+}
